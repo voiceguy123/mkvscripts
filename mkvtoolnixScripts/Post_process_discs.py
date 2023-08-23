3c595264d4ad236013a3a70e49fc62adf6007c9e
voiceguy123 <nicmoore@gmail.com>
2023-08-23 03:04:57 +0000

    Docker testing

--- mkvtoolnixScripts/Post_process_discs.py
+++ mkvtoolnixScripts/Post_process_discs.py
@@ -22,7 +22,8 @@ import shutil
 search_root = '/storage/Handbrake/Output/'
 # Destination root
 dest_root = '/storage/Media_'
-
+# Handbrake config/log directory
+hb_dir = '/storage/Handbrake_cfg/'
 # Disc types
 disc_types = ['DVD', 'BD', 'UHD']
 
@@ -54,7 +55,7 @@ def check_set_forced_subtitles(filename='', search_directory='', tracks=[]):
             if track['Default'] == 'Yes' and track['Forced'] == 'Yes':
                 # print(output)
                 # Run mkvpropedit to remove forced flags
-                print('Forced subtitles found: ' + file)
+                print('Removing forced subtitles found: ' + file, end='')
                 # Use track number instead of track name from mediainfo, track number is more reliable
                 mkvpropedit_subtitles = subprocess.Popen(['/usr/bin/mkvpropedit', search_directory + '/' + filename, '--edit', 'track:' + track['ID'], '--set', 'flag-forced=0', '--set', 'flag-default=0'], stdout=subprocess.PIPE)
                 # Wait for mkvpropedit to finish
@@ -62,9 +63,9 @@ def check_set_forced_subtitles(filename='', search_directory='', tracks=[]):
                 # # Read output from mkvpropedit and convert from bytes to string
                 mkvpropedit_subtitles_output = mkvpropedit_subtitles.stdout.read().decode('utf-8')
                 if 'Done.' in mkvpropedit_subtitles_output:
-                    print('Done.')
+                    print(' Done.')
                 else:
-                    print('Error: ' + mkvpropedit_subtitles_output)
+                    print(' Error: ' + mkvpropedit_subtitles_output)
 
 def check_set_title(filename='', search_directory='', track0={}):
     # Determine if file is a movie or a TV show by looking for the pattern SxxExx to determine full title
@@ -81,7 +82,7 @@ def check_set_title(filename='', search_directory='', track0={}):
     if 'Title' in track0 and track0['Title'] == title:
         pass
     else:
-        print('Setting title: ' + filename)
+        print('Setting title: ' + title, end='')
         # Run mkvpropedit to set title
         mkvpropedit_title = subprocess.Popen(['/usr/bin/mkvpropedit', search_directory + '/' + filename, '--edit', 'info', '--set', 'title=' + title], stdout=subprocess.PIPE)
         # Wait for mkvpropedit to finish
@@ -89,9 +90,9 @@ def check_set_title(filename='', search_directory='', track0={}):
         # # Read output from mkvpropedit and convert from bytes to string
         mkvpropedit_title_output = mkvpropedit_title.stdout.read().decode('utf-8')
         if 'Done.' in mkvpropedit_title_output:
-            print('Done.')
+            print(' Done.')
         else:
-            print('Error: ' + mkvpropedit_title_output)
+            print(' Error: ' + mkvpropedit_title_output)
     return is_tv, title
 
 def check_set_audio_tracks(filename='', search_directory='', tracks=[]):
@@ -125,11 +126,10 @@ def check_set_audio_tracks(filename='', search_directory='', tracks=[]):
                 channels = str(int(track['Channels']) -1) + '.1'
             # Determine if track name is correct
             track_name = track_name_codec + ' ' + channels
-            print(track_name)
             if track['Title'] == track_name:
                 pass
             else:
-                print('Setting track name: ' + file + ' Track: ' + track['ID'] + ' Name: ' + track_name)
+                print('Setting track name for track#: ' + track['ID'] + ' Name: ' + track_name, end='')
                 # Run mkvpropedit to set track name
                 mkvpropedit_track_name = subprocess.Popen(['/usr/bin/mkvpropedit', search_directory + '/' + filename, '--edit', 'track:' + track['ID'], '--set', 'name=' + track_name], stdout=subprocess.PIPE)
                 # Wait for mkvpropedit to finish
@@ -137,12 +137,11 @@ def check_set_audio_tracks(filename='', search_directory='', tracks=[]):
                 # # Read output from mkvpropedit and convert from bytes to string
                 mkvpropedit_track_name_output = mkvpropedit_track_name.stdout.read().decode('utf-8')
                 if 'Done.' in mkvpropedit_track_name_output:
-                    print('Done.')
+                    print(' Done.')
                 else:
-                    print('Error: ' + mkvpropedit_track_name_output)
+                    print(' Error: ' + mkvpropedit_track_name_output)
 
 def move_file(filename='', is_tv=False, title='', dest_dir=''):
-    print('Moving file: ' + filename)
     if is_tv:
         # Get season number from file name section SxxEyy and extract xx, strip leading 0 on season number if present
         show_name = title.split(':')[0]
@@ -155,12 +154,10 @@ def move_file(filename='', is_tv=False, title='', dest_dir=''):
             os.mkdir(dest_dir + '/' + show_name + '/Season ' + season_num)
             # Set destination directory to show/season directory
             dest_dir = dest_dir + '/' + show_name + '/Season ' + season_num
-        # Move file to destination location
-        print(dest_dir + '/' + filename)
-        shutil.move(search_dir + '/' + filename, dest_dir + '/' + filename)
-    else:
-        print(dest_dir + '/' + filename)
-        shutil.move(search_dir + '/' + filename, dest_dir + '/' + filename)
+    # Move file to destination location
+    print('Moving file: ' + filename + ' to: ' + dest_dir + '/' + filename, end='')
+    shutil.move(search_dir + '/' + filename, dest_dir + '/' + filename)
+    print(' Done.')
 
 
 # Process files for each disc type
@@ -173,6 +170,7 @@ for disc_type in disc_types:
     # Loop through list of valid files
     for file in file_list:
         # Run mediainfo on file
+        print('Running mediainfo on file: ' + file)
         mediainfo = subprocess.Popen(['/usr/bin/mediainfo', search_dir + '/' + file, '--Output=JSON'], stdout=subprocess.PIPE)
         # Read output from mediainfo and convert from bytes to string and save JSON to variable
         # Convert JSON to dictionary
